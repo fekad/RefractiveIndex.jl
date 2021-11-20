27f4337e54ab76c35471a95f01b4ce2e0b6a4f26
fekad <adam@fekete.co.uk>
2021-11-20 22:38:00 +0100

    fix testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,59 +1,48 @@
-using RefractiveIndex
+using RefractiveIndexDatabase
 using Test
 
 
+@testset "RefractiveIndexDatabase.jl" begin
+    @testset "Dispersion formulas" begin
+        # Sellmeier
+        @test isapprox(get_material("main", "Ar", "Grace-liquid-90K")(0.3885), 1.22809258, rtol=1e-6)
 
-# path = "src/Johnson.yml"
-# path = "src/BAF2.yml"
-# m = load_file(path)
-#
-# path = "https://refractiveindex.info/database/data/main/Ag/Johnson.yml"
-# load_url(path)
-#
+        # Sellmeier-2
+        @test isapprox(get_material("main", "CdTe", "Marple")(1.68), 2.72726378, rtol=1e-6)
 
-# function midrange(material)
-#     λmin, λmax = material.λrange
-#     return λmin + 0.5(λmax - λmin)
-# end
-#
-# function testRM(material, n_ref)
-#     n = material(midrange(material))
-#     # Compare only fractional parts
-#     isapprox(n_ref % 1, n % 1, rtol=1e-3)
-# end
-#
-# @testset "RefractiveIndex.jl" begin
-#     @testset "Dispersion formulas" begin
-#         # Sellmeier
-#         @test testRM(RefractiveMaterial("main", "Ar", "Grace-liquid-90K"), 1.2281)
-#
-#         # Sellmeier-2
-#         @test testRM(RefractiveMaterial("main", "CdTe", "Marple"), 2.7273)
-#
-#         # Polynomial
-#         @test testRM(RefractiveMaterial("other", "Mg-LiTaO3", "Moutzouris-o"), 2.1337)
-#
-#         # RefractiveIndex.INFO
-#         @test testRM(RefractiveMaterial("main", "ZnTe", "Li"), 2.6605)
-#
-#         # Cauchy
-#         @test testRM(RefractiveMaterial("main", "SF6", "Vukovic"), 1.00072071)
-#
-#         # Gases
-#         @test testRM(RefractiveMaterial("main", "He", "Mansfield"), 1.000034724)
-#
-#         # Herzberger
-#         @test testRM(RefractiveMaterial("main", "Si", "Edwards"), 3.4208)
-#
-#         # Retro
-#         @test testRM(RefractiveMaterial("main", "AgBr", "Schröter"), 2.2600)
-#
-#         # Exotic
-#         @test testRM(RefractiveMaterial("organic", "urea","Rosker-e"), 1.6000)
-#     end
-#
-#     @testset "Tabular data" begin
-#         # RefractiveNK
-#         @test testRM(RefractiveMaterial("main", "ZnO", "Stelling"), 1.5970)
-#     end
-# end
+        # Polynomial
+        @test isapprox(get_material("other", "Mg-LiTaO3", "Moutzouris-o")(1.0), 2.13370331, rtol=1e-6)
+
+        # RefractiveIndex.INFO
+        @test isapprox(get_material("main", "ZnTe", "Li")(15.275), 2.66048474, rtol=1e-6)
+
+        # Cauchy
+        @test isapprox(get_material("main", "SF6", "Vukovic")(0.95), 1.00072070, rtol=1e-6)
+
+        # Gases
+        @test isapprox(get_material("main", "He", "Mansfield")(1.26935), 1.00003472, rtol=1e-6)
+
+        # Herzberger
+        @test isapprox(get_material("main", "Si", "Edwards")(13.71865), 3.42084598, rtol=1e-6)
+
+        # Retro
+        @test isapprox(get_material("main", "AgBr", "Schröter")(0.5825), 2.26004419, rtol=1e-6)
+
+        # Exotic
+        @test isapprox(get_material("organic", "urea", "Rosker-e")(0.68), 1.60004980, rtol=1e-6)
+    end
+
+    @testset "Tabular data" begin
+        # RefractiveNK
+        @test isapprox(get_material("main", "ZnO", "Stelling")(0.866), 1.59478654 + 0.01177447im,  rtol=1e-6)
+    end
+
+    @testset "Online data" begin
+
+        path = "https://refractiveindex.info/database/data/main/Ag/Johnson.yml"
+        @test isapprox(load_url(path)(0.49), 0.05087409 + 3.04074578im,  rtol=1e-6)
+
+    end
+
+end
+
